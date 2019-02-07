fecad2d00ad6e0148eb09cd6b5acd6267ae54132
hckrpatrick <patricksanchez2555@gmail.com>
2019-02-07 15:55:22 +0800

    added license in the files

--- iskolendar/urls.py
+++ iskolendar/urls.py
@@ -1,3 +1,4 @@
+#This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2015-2016
 from django.contrib import admin
 from django.urls import path, include
 
