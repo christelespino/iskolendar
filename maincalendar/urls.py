df84afd552c6a113fabaf8f202e3b4300dcecb7c
artanquintic <amielle.allure09@gmail.com>
2019-02-21 23:08:38 +0800

    Without loginerror

--- maincalendar/urls.py
+++ maincalendar/urls.py
@@ -7,14 +7,16 @@ Patrick Joseph Sanchez
 Christel Anne Espino
 	added the url paths for updating event details and deleting event	
 '''
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.models import User
+from django.contrib.admin.views.decorators import staff_member_required
 from .views import EventDetailView, EventCreateView, EventUpdateView, EventDeleteView
 from . import views
 
 urlpatterns = [
     #path('', EventListView.as_view(), name='maincalendar'),
     path('event/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
-    path('event/new/', EventCreateView.as_view(), name='event-create'),
+    path('event/new/', staff_member_required(EventCreateView.as_view()), name='event-create'),
     path('event/<int:pk>/update', EventUpdateView.as_view(), name='event-update'),
     path('event/<int:pk>/delete', EventDeleteView.as_view(), name='event-delete'),
     path('', views.maincalendar, name = 'maincalendar'),
--- users/tests.py
+++ users/tests.py
@@ -1,13 +1,4 @@
 #This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2015-2016
-'''
-02/05/2019
-	Patrick Joseph Sanchez
-		made the Events visible in the admin page
-'''
+from django.test import TestCase
 
-from django.contrib import admin
-from .models import Event
-
-admin.site.register(Event)
-
-# Register your models here.
+# Create your tests here.
