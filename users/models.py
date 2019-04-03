32073e8cfc256df7807e870f8c3a5a3853297a41
christelespino <caespino2@up.edu.ph>
2019-04-04 02:19:32 +0800

    integrate bookmarks / fix register

--- users/models.py
+++ users/models.py
@@ -7,6 +7,28 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from maincalendar.models import Event
+
+degprogram = [
+	("1", "Chemical Engineering"),
+	("2", "Civil Engineering"),
+	("3", "Computer Science"),
+	("4", "Computer Engineering"),
+	("5", "Electrical Engineering"),
+	("6", "Electronics Engineering"),
+	("7", "Geodetic Engineering"),
+	("8", "Industrial Engineering"),
+	("9", "Mechanical Engineering"),
+	("10", "Mining Engineering"),
+	("11", "Metallurgical Engineering"),
+	("12", "Materials Engineering"),
+	("13", "Biology")
+]
+
+COLLEGE_CHOICES = (
+	("1", "College of Engineering"),
+	("2", "College of Science"),
+)
 
 class Profile(models.Model):
 	student_number = models.CharField(max_length = 100, default="", null = False)
@@ -14,6 +36,7 @@ class Profile(models.Model):
 	position = models.CharField(max_length = 100, default="", null = False)
 	organization = models.CharField(max_length = 100, default="", null = False)
 	org_acronym = models.CharField(max_length = 10, default="", null = False)
+	college = models.CharField(max_length = 100, default="", null = False, choices = COLLEGE_CHOICES)
 	degree_program = models.CharField(max_length = 100, default="", null = False)
 
 	user = models.OneToOneField(
@@ -23,11 +46,13 @@ class Profile(models.Model):
         related_name = "profile",
     )
 
+	bookmarks = models.ManyToManyField(Event, blank=True, related_name = "bookmarked")
+	
 	def __str__(self):
 		return self.name
 	'''
 	def get_absolute_url(self):
 		return reverse('event-detail', kwargs = {'pk': self.pk})
 	'''
-
+ 
 # Create your models here.
